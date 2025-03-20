72d20d35075599d97327fc0c665c8bbb43d19169
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-03-20 16:14:32 -0300

    feat: persit data on database file

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import http from 'node:http';
 import { json } from './middleware/json.js';
+import { Database } from './database.js';
 
-const users = [];
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -9,15 +10,18 @@ const server = http.createServer(async (req, res) => {
   await json(req, res);
 
   if (method == 'GET' && url == '/users') {
+    const users = database.select('users');
     return res.end(JSON.stringify(users));
   }
   if (method == 'POST' && url == '/users') {
     const { name, email } = req.body;
-    users.push({
+    const user = {
       id: 1,
       name,
       email,
-    });
+    };
+
+    database.insert('users', user);
 
     return res.writeHead(201).end();
   }
