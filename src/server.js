309c895396eb4b3c5c2fab2dca5e92ae22556a60
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-03-20 19:58:33 -0300

    feat: return the paramters from url

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import http from 'node:http';
 import { json } from './middleware/json.js';
 import { Database } from './database.js';
 import { routes } from './routes.js';
+import { extractQueryParams } from './util/extract-query-params.js';
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -15,7 +16,10 @@ const server = http.createServer(async (req, res) => {
   if (route) {
     const routeParams = req.url.match(route.path);
 
-    req.params = { ...routeParams.groups };
+    const { query, ...params } = routeParams.groups;
+
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
 
     return route.handler(req, res);
   }
---
+++ src/util/extract-query-params.js
@@ -0,0 +1,10 @@
+export function extractQueryParams(query) {
+  return query
+    .substr(1)
+    .split('&')
+    .reduce((queryParam, param) => {
+      const [key, value] = param.split('=');
+      queryParam[key] = value;
+      return queryParam;
+    }, {});
+}
